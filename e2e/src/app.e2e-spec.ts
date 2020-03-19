99e6403f73c10894f503666dd3e5f9f25e60b4de
Mouadh Ben Braham <bbmouadh90@gmail.com>
2020-03-19 15:54:08 +0100

    [FEATURE][SER-116] Add gitlab-ci

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,7 +8,7 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display WIDIGITAL', () => {
     page.navigateTo();
     expect(page.getTitleText()).toEqual('WIDIGITAL !');
   });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,7 +16,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-bootstrap'`, () => {
+  it(`should have as title 'WIDIGITAL'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('WIDIGITAL');
