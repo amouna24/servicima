f61b9308b390f08528d525a1964cc83263d18c17
hamza.jridi <hamza.jridi@widigital-group.com>
2020-06-07 21:07:08 +0200

    [FEATURE][SER-XXX] add the can be displayed directive

---
+++ src/app/shared/directives/can-be-displayed.directive.spec.ts
@@ -0,0 +1,50 @@
+import { Component, DebugElement, ElementRef, TemplateRef, ViewContainerRef } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+
+import { CanBeDisplayedDirective } from './can-be-displayed.directive';
+
+@Component({
+  selector: 'wid-my-test-component',
+  template: `
+    <div *canBeDisplayed="{feature: 'TEST', license: '123456'}"></div>
+    <div *canBeDisplayed="{feature: 'dashboard', license: '123456'}"></div>
+  `,
+})
+class TestComponent { }
+
+fdescribe('canBeDisplayed', () => {
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  let divEls: DebugElement[];
+
+  class MockElementRef implements ElementRef {
+    nativeElement = { };
+  }
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: ElementRef, useValue: new MockElementRef() },
+        TemplateRef,
+        ViewContainerRef,
+      ],
+      declarations: [ TestComponent, CanBeDisplayedDirective ],
+    })
+      .compileComponents().then(() => {
+      fixture = TestBed.createComponent(TestComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+      divEls = fixture.debugElement.queryAll(By.css('div'));
+    });
+  }));
+
+  it('should create an instance', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should has one div in template of my TestComponent', () => {
+    expect(divEls.length).toEqual(1);
+  });
+});
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { MaterialModule } from './material/material.module';
+
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { HeaderComponent } from './components/header/header.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { SpinnerComponent } from './components/spinner/spinner.component';
-import { RouterModule } from '@angular/router';
 import { TranslationPipe } from './pipes/translation/translation.pipe';
+import {CanBeDisplayedDirective} from './directives/can-be-displayed.directive';
 
 @NgModule({
-  declarations: [SidenavComponent, HeaderComponent, SpinnerComponent, TranslationPipe],
+  declarations: [
+    /* Components */
+    SidenavComponent,
+    HeaderComponent,
+    SpinnerComponent,
+    /* Directives */
+    CanBeDisplayedDirective,
+    /* Pipes */
+    TranslationPipe
+  ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
