7ce42135cf3bb6de5b3df569938918996b239a43
Amine SBOUI <amine.sboui@widigital-group.com>
2020-09-07 09:48:06 +0100

    [BUGFIX][SER-000] Fix Contract

---
+++ src/app/core/services/sheet/sheet.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SheetService {
+
+  appSheets: Array<{ sheetName: string, sheetComponent: any }>;
+  uploadSheetActionResponse$ = new Subject<string>();
+
+  constructor(
+    private _bottomSheet: MatBottomSheet,
+  ) { }
+
+  /**************************************************************************
+   * @description Register Sheets to an array
+   *************************************************************************/
+  registerSheets(sheetsList: Array<{ sheetName: string, sheetComponent: any }>) {
+    this.appSheets = sheetsList;
+  }
+
+  /**************************************************************************
+   * @description Get Sheet by Name
+   *************************************************************************/
+  getSheetComponentRef(sheetName: string) {
+    const sheet = this.appSheets.find(sheetElement => sheetElement.sheetName === sheetName);
+    return sheet.sheetComponent;
+  }
+
+  displaySheet(sheetName: string, sheetData?: object): Observable<any> {
+    const sheetComponent = this.getSheetComponentRef(sheetName);
+    const dialogRef = this._bottomSheet.open(sheetComponent, {
+      data: sheetData
+    });
+    return dialogRef.afterDismissed();
+  }
+
+}
