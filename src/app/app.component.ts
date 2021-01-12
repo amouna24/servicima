392e8ad3eff417a3028beb665fdc71ebb090d588
iheb MEJRI <iheb.mejri@widigital-group.com>
2021-01-12 11:39:38 +0100

    [FEATURTE][SER-126] Svg icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,10 +18,20 @@ export class AppComponent implements OnInit {
   ];
   title = 'WIDIGITAL ' + environment.env;
   iconsList = [
-    { name: 'buy', path: 'assets/icons/bag.svg'},
-    { name: 'wi-dashboared', path: 'assets/icons/Dashbored.svg'},
-    { name: 'wi_contracts_management', path: 'assets/icons/contracts_management.svg'},
-    { name: 'wi_customer', path: 'assets/icons/Customer.svg'},
+    { name: 'wi-bag', path: 'assets/icons/bag.svg'},
+    { name: 'wi-pen', path: 'assets/icons/pen-solid.svg'},
+    { name: 'wi-eye', path: 'assets/icons/eye-solid.svg'},
+    { name: 'wi-archive', path: 'assets/icons/user-slash-solid.svg'},
+    { name: 'wi-marker', path: 'assets/icons/marker-solid.svg'},
+    /* Side Nav */
+    { name: 'wi-dashboard', path: 'assets/icons/dashboard.svg'},
+    { name: 'wi-recruitment', path: 'assets/icons/recruitment.svg'},
+    { name: 'wi-HR', path: 'assets/icons/HR.svg'},
+    { name: 'wi-placement', path: 'assets/icons/placement.svg'},
+    { name: 'wi-activity-reports', path: 'assets/icons/Activity_Reports.svg'},
+    { name: 'wi-contract-management', path: 'assets/icons/Contract_Management.svg'},
+    { name: 'wi-billings', path: 'assets/icons/Billing.svg'},
+    /* Alert Icons */
     { name: 'wi_alert_error', path: 'assets/icons/error.svg'},
     { name: 'wi_alert_success', path: 'assets/icons/success.svg'},
     { name: 'wi_alert_info', path: 'assets/icons/info.svg'},
@@ -32,6 +42,12 @@ export class AppComponent implements OnInit {
     { name: 'wi_user', path: 'assets/icons/user-solid.svg', viewBox: '0 -10 20 45'},
     { name: 'wi_settings', path: 'assets/icons/cogs-solid.svg', viewBox: '0 -15 20 45'},
     { name: 'wi_logout', path: 'assets/icons/sign-out-alt-solid.svg', viewBox: '0 -15 20 45'},
+    { name: 'wi_theme', path: 'assets/icons/palette-solid.svg', viewBox: '0 -15 20 45'},
+    /*Social media icons*/
+    { name: 'wi_whatsapp', path: 'assets/icons/whatsapp-square-brands.svg'},
+    { name: 'wi_youtube', path: 'assets/icons/youtube-square-brands.svg'},
+    { name: 'wi_twitter', path: 'assets/icons/twitter-square-brands.svg'},
+    { name: 'wi_linkedin', path: 'assets/icons/linkedin-brands.svg'},
   ];
   constructor(
     private authService: AuthService,
