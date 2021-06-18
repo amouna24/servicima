023655d7a9c95a3f17d98f8775f77bae9ffb4596
Amine SBOUI <amine.sboui@widigital-group.com>
2021-06-18 17:53:15 +0100

    last commit

--- src/app/shared/models/contractProject.model.ts
+++ src/app/shared/models/contractProject.model.ts
@@ -4,15 +4,16 @@ export interface IContractProject {
   /* ID */
   _id?: string;
   ContractProjectKey?: IContractProjectKey;
+  category_code?: number;
   project_desc: string;
   start_date: Date;
   end_date?: Date;
-  project_status?: string;
-  comment: string;
-  category_code?: number;
   project_rate?: string;
   rate_currency?: string;
   vat_nbr?: string;
+  project_status?: string;
+  comment: string;
+
   application_id: string;
   company_email: string;
   contract_code: string;
