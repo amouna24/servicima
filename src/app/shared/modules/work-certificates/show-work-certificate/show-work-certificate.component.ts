e4894eab769f04c22f194d98170d018e438c35c6
brahim hmida <brahim.hmida@widigital-group>
2021-12-30 15:12:29 +0100

    fix show certification

--- src/app/shared/modules/work-certificates/show-work-certificate/show-work-certificate.component.ts
+++ src/app/shared/modules/work-certificates/show-work-certificate/show-work-certificate.component.ts
@@ -14,15 +14,21 @@ import { ModalService } from '@core/services/modal/modal.service';
 import { Subscription } from 'rxjs';
 import { SignatureCertificateComponent } from '@shared/modules/work-certificates/signature-certificate/signature-certificate.component';
 import { IUserInfo } from '@shared/models/userInfo.model';
+import { IViewParam } from '@shared/models/view.model';
+import {  dataAppearance } from '@shared/animations/animations';
 
 @Component({
   selector: 'wid-show-work-certificate',
   templateUrl: './show-work-certificate.component.html',
-  styleUrls: ['./show-work-certificate.component.scss']
+  styleUrls: ['./show-work-certificate.component.scss'],
+  animations: [
+    dataAppearance,
+  ]
 })
 export class ShowWorkCertificateComponent implements OnInit {
 
   certificate: any;
+  nationality = '';
   user: IUserModel;
   position: string;
   requestType: string;
@@ -40,6 +46,7 @@ export class ShowWorkCertificateComponent implements OnInit {
   emailAddress: string;
   companyName: string;
   infoUser: IUserInfo;
+  nationalitiesList: IViewParam[] = [];
 
   /**************************************************************************
    * @description get initial data
@@ -65,11 +72,13 @@ export class ShowWorkCertificateComponent implements OnInit {
     await this.getConnectedUser();
 
     if (this.certificate.request_type === 'CERT') {
-      this.requestType = 'Certificate';
-    } else { this.requestType = 'Expatriation'; }
+      this.requestType = 'rh_title_certif';
+    } else { this.requestType = 'rh_exp_certif'; }
     await  this.getRefData();
   }
-
+  /**************************************************************************
+   * @description get Ref data
+   *************************************************************************/
   async getRefData() {
     /********************************** REF DATA **********************************/
     this.refData = await this.refDataService.getRefData(
@@ -79,6 +88,16 @@ export class ShowWorkCertificateComponent implements OnInit {
       ['PROF_TITLES'],
       false
     );
+    this.position = this.refData['PROF_TITLES'].filter(x => x.value === this.certificate.title_id)[0].viewValue;
+
+    this.utilsService
+      .getNationality(this.utilsService
+        .getCodeLanguage( this.localStorageService.getItem('language')['langId'])).map((nationality) => {
+      this.nationalitiesList.push({ value: nationality.NATIONALITY_CODE, viewValue: nationality.NATIONALITY_DESC });
+    });
+    if (this.certificate.nationality_id) {
+      this.nationality = this.nationalitiesList.filter(x => x.value === this.certificate.nationality_id)[0].viewValue;
+    }
   }
   /**************************************************************************
    * @description confirm certification
