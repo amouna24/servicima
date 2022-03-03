52f70649b3a0037af8f6b88c6d6f944eb0967614
Dhia <dhia.othmen@widigital-group.com>
2022-03-03 09:18:56 +0100

    [FEATURE][SER-111] Add test session info

--- src/app/home/modules/manager/modules/test-management/test-management-routing.module.ts
+++ src/app/home/modules/manager/modules/test-management/test-management-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { BlocListComponent } from './bloc-list/bloc-list.component';
+import { SessionInfoComponent } from './session-info/session-info.component';
 
 const routes: Routes = [
   {
     path: 'bloc-list',
     component: BlocListComponent
+  },
+
+  {
+    path: 'session-info',
+    component: SessionInfoComponent
   }
 ];
 
--- src/app/home/modules/manager/modules/test-management/test-management.module.ts
+++ src/app/home/modules/manager/modules/test-management/test-management.module.ts
@@ -5,9 +5,10 @@ import { SharedModule } from '@shared/shared.module';
 import { TestManagementRoutingModule } from './test-management-routing.module';
 import { BlocListComponent } from './bloc-list/bloc-list.component';
 import { BlocListModalComponent } from './bloc-list-modal/bloc-list-modal.component';
+import { SessionInfoComponent } from './session-info/session-info.component';
 
 @NgModule({
-  declarations: [BlocListComponent, BlocListModalComponent],
+  declarations: [BlocListComponent, BlocListModalComponent, SessionInfoComponent],
   imports: [
     CommonModule,
     TestManagementRoutingModule,
