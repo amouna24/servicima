cacfd66c3a4d083fc79484e10903edeb4dd32443
brahim hmida <brahim.hmida@widigital-group>
2022-03-21 08:43:14 +0100

    [BUGFIX][WID-61] fix contractor integration

--- src/app/home/modules/candidate/modules/test-management/test-management.module.ts
+++ src/app/home/modules/candidate/modules/test-management/test-management.module.ts
@@ -6,10 +6,9 @@ import { TestManagementRoutingModule } from './test-management-routing.module';
 import { TestQcmComponent } from './test-qcm/test-qcm.component';
 import { WelcomeToTestComponent } from './welcome-to-test/welcome-to-test.component';
 import { TestCongratulationsComponent } from './test-congratulations/test-congratulations.component';
-import { DialogInviteComponent } from './dialog-invite/dialog-invite.component';
 
 @NgModule({
-  declarations: [ TestQcmComponent, WelcomeToTestComponent, TestCongratulationsComponent, DialogInviteComponent ],
+  declarations: [ TestQcmComponent, WelcomeToTestComponent, TestCongratulationsComponent ],
   imports: [
     CommonModule,
     TestManagementRoutingModule,
